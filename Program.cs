7a1477439586cc4c75c7dd84dd200974397fba93
Ira <airichka@mail.ru>
2022-12-04 20:29:29 +0400

    ws1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int a;
+int b;
+
+Console.Write($"Введите первое число: ");
+int.TryParse(Console.ReadLine()!, out a); 
+Console.Write($"Введите второе число: ");
+int.TryParse(Console.ReadLine()!, out b); 
+
+int index = 0;
+
+while (index <= 5)
+{Console.Write($"{index} ");
+index ++;}
+
+Console.Write($"\n");
+for (int index2 = 0; index < 5; index ++ )
+
+{Console.Write($"{index2} ");
+}
